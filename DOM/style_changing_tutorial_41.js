53766427e01cd3a95977180a7fc13edcda4f730d
Md.Ruhul Amin <mdruhulamin534793@gmail.com>
2024-12-20 16:25:56 +0600

    changing CSS style using DOM

---
+++ DOM/style_changing_tutorial_41.js
@@ -0,0 +1,10 @@
+var myStyle = document.getElementById("paraId");
+
+function addStyle()
+{
+    myStyle.classList.add("paragraph-style");
+}
+function removeStyle()
+{
+    myStyle.classList.remove("paragraph-style");
+}
